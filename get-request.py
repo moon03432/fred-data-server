4c0139315f0d6ff335e6c1a23c2e1186a11dead3
tyrion huang <tyrion.huang@hpe.com>
2017-05-08 08:58:20 +0800

    update

--- get-request.py
+++ get-request.py
@@ -1,16 +1,18 @@
 import requests
+import json
 
-# response = requests.get('https://c9t12389.itcs.hpecorp.net:35358/', verify=False)
-# print(response.text)
+no_text = "monetary"
 
 payload = {
-    "search_text": 'bank',
+    "search_text": "healthcare equipement",
     "api_key": '686499b1e93cfa261ddef9faa553f4b9',
-    "file_type": 'json',
-    "limit": 1
+    "file_type": 'json'
 }
-#url = 'https://api.stlouisfed.org/fred/series/search?search_text=' + 'bank' + '&api_key=686499b1e93cfa261ddef9faa553f4b9&file_type=json'
+
 url = 'https://api.stlouisfed.org/fred/series/search'
 
 response = requests.get(url, params=payload, verify=False)
-print(response.text)
+
+data = json.dumps(response.text)
+
+print(data)
